027c389d254bc6b59a85ad60efb981b7b98d86e9
TheHedgehock <tomas.hedgehock@gmail.com>
2021-05-02 13:09:51 +0200

    upload

---
+++ BetterPage.js
@@ -0,0 +1,32 @@
+// Licensed to TheHedgehock
+
+var BetterPage = {
+    includeHtml: function() {
+        var z, i, elmnt, file, xhttp;
+        prefix = "BetterPage > ";
+        
+        z = document.getElementsByTagName("*");
+        for (i = 0; i < z.length; i++) {
+            elmnt = z[i];
+            
+            file = elmnt.getAttribute("include-html");
+            if (file) {
+                
+                xhttp = new XMLHttpRequest();
+                xhttp.onreadystatechange = function() {
+                    if (this.readyState == 4) {
+                        if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+                        if (this.status == 404) {elmnt.innerHTML = prefix + "Page not found";}
+                    
+                        elmnt.removeAttribute("w3-include-html");
+                        BetterPage.includeHtml();
+                    }
+                }
+                xhttp.open("GET", file, true);
+                xhttp.send();
+                
+                return;
+            }
+        }
+    }
+}
